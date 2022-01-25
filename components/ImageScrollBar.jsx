bb0a0ad3d3487dc1da34e1887f1ded4f6196cef5
Jonmari <jonmari@simplecloudology.com>
2022-01-26 00:01:23 +0800

    final changes

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -1,4 +1,5 @@
-import { Flex, Icon } from "@chakra-ui/react";
+import { Flex, Icon, Box } from "@chakra-ui/react";
+import Image from "next/image";
 import React, { useContext } from "react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
@@ -10,9 +11,10 @@ const LeftArrow = () => {
         <Flex justifyContent="center" alignItems="center" marginRight="1">
             <Icon
                 as={FaArrowAltCircleLeft}
-                onClick={scrollPrev}
+                onClick={() => scrollPrev()}
                 fontSize="2xl"
                 cursor="pointer"
+                d={["none", "none", "none", "block"]}
             />
         </Flex>
     );
@@ -22,24 +24,38 @@ const RightArrow = () => {
     const { scrollNext } = useContext(VisibilityContext);
 
     return (
-        <Flex justifyContent="center" alignItems="center" marginRight="1">
+        <Flex justifyContent="center" alignItems="center" marginLeft="1">
             <Icon
                 as={FaArrowAltCircleRight}
-                onClick={scrollNext}
+                onClick={() => scrollNext()}
                 fontSize="2xl"
                 cursor="pointer"
+                d={["none", "none", "none", "block"]}
             />
         </Flex>
     );
 };
 
-const ImageScrollBar = () => {
+const ImageScrollBar = ({ data }) => {
     return (
         <ScrollMenu
             LeftArrow={LeftArrow}
             RightArrow={RightArrow}
             style={{ overflow: "hidden" }}
-        ></ScrollMenu>
+        >
+            {data.map((item) => (
+                <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+                    <Image
+                        placeholder="blur"
+                        blurDataURL={item.url}
+                        src={item.url}
+                        width={1000}
+                        height={500}
+                        sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+                    />
+                </Box>
+            ))}
+        </ScrollMenu>
     );
 };
 
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,5 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import millify from "millify";
 import React from "react";
+import { BsGridFill } from "react-icons/bs";
+import { FaBath, FaBed } from "react-icons/fa";
+import { GoVerified } from "react-icons/go";
 import ImageScrollBar from "../../components/ImageScrollBar";
 import { baseUrl, fetchApi } from "../../utils/fetchApi";
 
@@ -15,11 +19,124 @@ const PropertyDetails = ({
         purpose,
         amenities,
         photos,
+        agency,
+        bath,
+        type,
+        furnishingStatus,
     },
 }) => {
     return (
         <Box maxWidth="1000px" margin="auto" p="4">
-            {photos && <ImageScrollBar />}
+            {photos && <ImageScrollBar data={photos} />}
+            <Box width="full" p="6">
+                <Flex
+                    paddingTop="2"
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Flex alignItems="center">
+                        <Box paddingRight="3" color="green.400">
+                            {isVerified && <GoVerified />}
+                        </Box>
+                        <Text fontWeight="bold" fontSize="lg">
+                            AED {millify(price)}
+                            {rentFrequency && `/${rentFrequency}`}
+                        </Text>
+                    </Flex>
+                    <Box>
+                        <Avatar size="sm" src={agency?.logo?.url} />
+                    </Box>
+                </Flex>
+                <Flex
+                    alignItems="center"
+                    p="1"
+                    justifyContent="space-between"
+                    w="250px"
+                    color="blue.400"
+                >
+                    {rooms} <FaBed /> | {bath} <FaBath /> | {millify(area)} sqft{" "}
+                    <BsGridFill />
+                </Flex>
+                <Box marginTop="2">
+                    <Text fontSize="lg" marginBottom="2" fontWeight="bold">
+                        {title}
+                    </Text>
+                    <Text lineHeight="2" color="gray.600">
+                        {description}
+                    </Text>
+                    <Box>
+                        <Flex
+                            flexWrap="wrap"
+                            textTransform="uppercase"
+                            justifyContent="space-between"
+                        >
+                            <Flex
+                                justifyContent="space-between"
+                                w="400px"
+                                borderBottom="1px"
+                                borderColor="gray.100"
+                                p="3"
+                            >
+                                <Text>Type</Text>
+                                <Text fontWeight="bold">{type}</Text>
+                            </Flex>
+                            <Flex
+                                justifyContent="space-between"
+                                w="400px"
+                                borderBottom="1px"
+                                borderColor="gray.100"
+                                p="3"
+                            >
+                                <Text>Purpose</Text>
+                                <Text fontWeight="bold">{purpose}</Text>
+                            </Flex>
+                            {furnishingStatus && (
+                                <Flex
+                                    justifyContent="space-between"
+                                    w="400px"
+                                    borderBottom="1px"
+                                    borderColor="gray.100"
+                                    p="3"
+                                >
+                                    <Text>Furnishing Status</Text>
+                                    <Text fontWeight="bold">
+                                        {furnishingStatus}
+                                    </Text>
+                                </Flex>
+                            )}
+                        </Flex>
+                        <Box>
+                            {amenities.length && (
+                                <Text
+                                    fontWeight="black"
+                                    fontSize="2xl"
+                                    marginTop="5"
+                                >
+                                    Amenities
+                                </Text>
+                            )}
+                            <Flex flexWrap="wrap">
+                                {amenities.map((item) =>
+                                    item.amenities.map((amenity) => (
+                                        <Text
+                                            key={amenity.text}
+                                            fontWeight="bold"
+                                            color="blue.400"
+                                            fontSize="l"
+                                            p="2"
+                                            bg="gray.200"
+                                            m="1"
+                                            borderRadius="5"
+                                        >
+                                            {amenity.text}
+                                        </Text>
+                                    )),
+                                )}
+                            </Flex>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
         </Box>
     );
 };
